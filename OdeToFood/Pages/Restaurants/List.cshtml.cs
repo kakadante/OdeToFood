c87b6d48b40aeac1416ae92d8b22f3addf18a0df
kaka_dante <danielmasila76@gmail.com>
2020-02-05 15:33:17 +0300

    BUILDING A PAGE MODEL
    Tume Include Property ya RESTAURANTS na tukainvoke ili iGET - ALL --- After tunatengeneza RAZOR PAGES ya kuDISPLAY info
    
    DISPLAYING A TABLE OF RESTAURANTS
    
    ~ Tulicreate table VIEW kwa ile LIST VIEW

--- OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using OdeToFood.Core;
 using OdeToFood.Data;
 
 namespace OdeToFood.Pages.Restaurants
@@ -12,17 +13,23 @@ namespace OdeToFood.Pages.Restaurants
     public class ListModel : PageModel
     {
         private readonly IConfiguration _config;
+        private readonly IRestaurantData _restaurantData;
 
         public string Message { get; set; }
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+
 
         public ListModel(IConfiguration config, IRestaurantData restaurantData)
         {
             _config = config;
+            _restaurantData = restaurantData;
         }
 
         public void OnGet()
         {
             Message = _config["Message"];
+            Restaurants = _restaurantData.GetAll();
+
         }
     }
 }

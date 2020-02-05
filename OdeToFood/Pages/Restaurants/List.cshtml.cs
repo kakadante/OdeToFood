bfe616975185b4d48f69112e627013901ff1ea0c
kaka_dante <danielmasila76@gmail.com>
2020-02-05 14:47:59 +0300

    TUME-INJECT CODE FROM APPSETTINGS JSON TO LIST PageModel IKAKUA DISPLAYED KWA LIST view

--- OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -4,17 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace OdeToFood.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
+        private readonly IConfiguration _config;
 
         public string Message { get; set; }
 
+        public ListModel(IConfiguration config)
+        {
+            _config = config;
+        }
+
         public void OnGet()
         {
-            Message = "Sasa Dante";
+            Message = _config["Message"];
         }
     }
 }

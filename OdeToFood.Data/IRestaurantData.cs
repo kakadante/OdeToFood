29d2006a01253d4132d37409daf51da7d8dee70f
kaka_dante <danielmasila76@gmail.com>
2020-02-05 14:54:11 +0300

    CREATE ANOTHER CLASS LIBRARY - START FROM A NEW PROJECT Class Library(.Net Core) for ABSTRACTIONS/INTERFACES - In Memory Data Store
    ~ CREATE class InMemoryRestaurantData : IRestaurantData
    AND CTRL+ENTER - Implement Interface
    ~ Create a list of restaurants
    
    ~ And then GetAll() by
                return from r in restaurants
                    orderby r.Name
                    select r;

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant {Id = 1, Name = "Debonairs Pizza", Location = "Machakos", Cuisine = CuisineType.Mexican},
+                new Restaurant {Id = 2, Name = "Kuku", Location = "Nairobi", Cuisine = CuisineType.Italian},
+                new Restaurant {Id = 3, Name = "Chips", Location = "Naks", Cuisine = CuisineType.Indian},
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants
+                orderby r.Name
+                select r;
+        }
+    }
+}

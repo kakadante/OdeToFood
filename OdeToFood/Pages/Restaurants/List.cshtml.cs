b074c05fc01b0285e6b79185c18f530a1e14b4df
kaka_dante <danielmasila76@gmail.com>
2020-02-05 14:57:39 +0300

    BUILDING A DATA ACCESS SERVICE
    We Configured service kwa startup file then tukaicall kwa list pagemodel

--- OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using OdeToFood.Data;
 
 namespace OdeToFood.Pages.Restaurants
 {
@@ -14,7 +15,7 @@ namespace OdeToFood.Pages.Restaurants
 
         public string Message { get; set; }
 
-        public ListModel(IConfiguration config)
+        public ListModel(IConfiguration config, IRestaurantData restaurantData)
         {
             _config = config;
         }

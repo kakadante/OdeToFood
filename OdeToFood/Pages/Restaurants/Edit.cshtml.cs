a6ee1e9cb460a13b12c39f4b5e58281a440bb53d
kaka_dante <danielmasila76@gmail.com>
2020-02-18 15:16:46 +0300

    FOLLOWING THE POST-GET-REDIRECT PATTERN
    (P-R-G PATTERN)
    
    Ili isi-submit mara mbili - kaa ni credit card ingenyooshwa mara mbili that's why ata Browser inaconfirm FORM RE-SUBMISSION

--- OdeToFood/Pages/Restaurants/Edit.cshtml.cs
+++ OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -43,6 +43,7 @@ namespace OdeToFood
             {
                 _restaurantData.Update(Restaurant);
                 _restaurantData.Commit();
+                return RedirectToPage("./Detail", new {restaurantId = Restaurant.Id});
             }
             Cuisines = _htmlHelper.GetEnumSelectList<CuisineType>();
             return Page();

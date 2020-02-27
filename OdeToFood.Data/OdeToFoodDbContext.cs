9612fdfc10bf3f9a41a3c3cbb15c8027e844d38f
kaka_dante <danielmasila76@gmail.com>
2020-02-27 17:03:26 +0300

    USING THE ENTITY FRAMEWORK TOOLS
    USING OTHER DATABASES AND TOOLS
    
    ADDING CONNECTION STRINGS AND REGISTERING SERVICES
    
    ~ Kwa appsettings.json ndio tunaeka Connection string kawaida tu ya kupoint kwa DB = LocalDb ya VS ndio tunatumia
    
    ~ Kwa startup.cs tume-add ii code block
    services.AddDbContextPool.....
    
    ndio tumake OdeToFoodDbContext i-TALK with the LOCAL DB
    io base(options) ni baseclass ya DbContext
    
    ~ Sasa kwa DBCONTEXT tumeadd CONSTRUCTOR(ctor)

--- OdeToFood.Data/OdeToFoodDbContext.cs
+++ OdeToFood.Data/OdeToFoodDbContext.cs
@@ -8,6 +8,12 @@ namespace OdeToFood.Data
 {
     public class OdeToFoodDbContext : DbContext
     {
+        public OdeToFoodDbContext(DbContextOptions<OdeToFoodDbContext> options)
+        : base(options)
+        {
+                
+        }
+
         public DbSet<Restaurant> Restaurants { get; set; }
     }
 }
